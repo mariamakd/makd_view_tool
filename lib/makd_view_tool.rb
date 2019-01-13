55c8e5062e44256fa6857d270cc233ab394ae19c
Mariori <mariamakantasi@gmail.com>
2019-01-14 00:03:43 +0200

    MakdTool initial commit

---
+++ lib/makd_view_tool.rb
@@ -0,0 +1,7 @@
+require "makd_view_tool/version"
+require "makd_view_tool/renderer"
+
+module MakdViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/makd_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MakdViewTool
+  VERSION = "0.1.0"
+end
